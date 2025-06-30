dc41f3af5714f96d53102ee09ab8cb80b68156c2
Mekd422 <mekdes.asefa-ug@aau.edu.et>
2025-06-30 22:34:10 +0300

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { ProfilePage } from './pages/ProfilePage'
+import { LoginPage } from './pages/LoginPage'
 
 export const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element = {<HomePage/>}/>
+        <Route path="/login" element = {<LoginPage/>}/>
+        <Route path="/profile" element = {<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
